7b962b46b53359733ef8eaa648599262e3435d46
Saurabh Nagbanshi <75077374+krsaurabhmca@users.noreply.github.com>
2026-04-15 18:48:54 +0530

    Finalize projects API and cleanup debug scripts

--- api/projects.php
+++ api/projects.php
@@ -1,7 +1,5 @@
 <?php
 require_once 'auth_check.php';
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 $method = $_SERVER['REQUEST_METHOD'];
 $executive_id = $auth_user['id'];
--- api/test_projects.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'auth_check.php';
-echo json_encode(['success' => true, 'message' => 'API is reachable', 'user' => $auth_user]);
-?>
